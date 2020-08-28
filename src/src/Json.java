1c33d2149acf6c12e75283c90060a34c492cb1c1
Meeliste <meelis.teern@gmail.com>
2020-08-28 10:13:35 +0300

    Lesson1

--- src/src/Json.java
+++ src/src/Json.java
@@ -2,20 +2,38 @@ public class Json {
 
     https://jsonlint.com/ // Json validator
 
-    [{
+            [{
         "name": "John",
                 "age": 31,
                 "aadress": "Kesklinn",
-                "cars": ["Toyota", "Honda", "Mazda"]
+                "cars": [{
+            "mark": "Toyota",
+                    "number": "123ABC"
+        }, {
+            "mark": "Honda",
+                    "number": "234QWE"
+        }]
     }, {
         "name": "Mike",
                 "age": 32,
                 "aadress": "Mustmäe",
-                "cars": ["BMW", "Audi", "VW"]
+                "cars": [{
+            "mark": "BMW",
+                    "number": "345WER"
+        }, {
+            "mark": "Audi",
+                    "number": "456ERT"
+        }]
     }, {
         "name": "Tim",
                 "age": 33,
                 "aadress": "Nõmme",
-                "cars": ["Volvo", "Saab", "Jaguar"]
+                "cars": [{
+            "mark": "Volvo",
+                    "number": "567ASD"
+        }, {
+            "mark": "Saab",
+                    "number": "678SDF"
+        }]
     }]
 }
